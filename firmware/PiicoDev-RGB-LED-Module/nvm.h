3121817c0637e6e416e5fac2ca4fcbd69c84f7a1
Michael Ruppe <production.inbox@coreelectronics.com.au>
2021-08-14 10:23:23 +1000

    user controlled power led

--- firmware/PiicoDev-RGB-LED-Module/nvm.h
+++ firmware/PiicoDev-RGB-LED-Module/nvm.h
@@ -13,12 +13,6 @@ Distributed as-is; no warranty is given.
 
 //Location in EEPROM for each thing we want to store between power cycles
 enum eepromLocations {
-  LOCATION_I2C_ADDRESS = 0x00, //Device's address
-  // LOCATION_INTERRUPTS = 0x01,
-  // LOCATION_LED_BRIGHTNESS = 0x02,
-  // LOCATION_LED_PULSEGRANULARITY = 0x03,
-  // LOCATION_LED_PULSECYCLETIME = 0x04,
-  // LOCATION_LED_PULSEOFFTIME = 0x06,
-  // LOCATION_BUTTON_DEBOUNCE_TIME = 0x08,
-  LOCATION_ADDRESS_TYPE = 0x0A,
+  LOCATION_I2C_ADDRESS = 0x00,  // Device's address
+  LOCATION_ADDRESS_TYPE = 0x0A, // Address type can be either hardware defined (jumpers/switches), or software defined by user.
 };
