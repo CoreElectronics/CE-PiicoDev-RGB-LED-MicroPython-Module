0241854e002da8930c7f4cac27ef88d36b78f5b1
GitHub Action <action@github.com>
2021-09-04 08:47:33 +0000

    Update min/PiicoDev_RGB.py

--- min/PiicoDev_RGB.py
+++ min/PiicoDev_RGB.py
@@ -1,7 +1,7 @@
+_A='big'
 from PiicoDev_Unified import *
-from time import sleep
 i2c=PiicoDev_Unified_I2C()
-_baseAddr=8
+_baseAddr=32
 _DevID=132
 _regDevID=0
 _regFirmVer=1
@@ -9,20 +9,34 @@ _regCtrl=3
 _regI2cAddr=4
 _regBright=5
 _regLedVals=6
+def hsv_to_rgb(h,s=1,v=1):
+	A=1.0
+	if s==0.0:v*=255;return v,v,v
+	i=int(h*6.0);f=h*6.0-i;p,q,t=int(255*(v*(A-s))),int(255*(v*(A-s*f))),int(255*(v*(A-s*(A-f))));v*=255;i%=6
+	if i==0:return[v,t,p]
+	if i==1:return[q,v,p]
+	if i==2:return[p,v,t]
+	if i==3:return[p,q,v]
+	if i==4:return[t,p,v]
+	if i==5:return[v,p,q]
 class PiicoDev_RGB:
 	def setPixelColor(self,n,r,g,b):self.led[n]=[r,g,b]
 	def show(self):buffer=bytes(self.led[0])+bytes(self.led[1])+bytes(self.led[2]);i2c.writeto_mem(self.addr,_regLedVals,buffer)
-	def setBrightness(self,x):self.bright=x;self.i2c.writeto_mem(self.addr,_regBright,bytes([self.bright]));sleep(0.001)
-	def clear(self):self.i2c.writeto_mem(self.addr,_regCtrl,bytes([1]));sleep(0.001)
+	def setBrightness(self,x):self.bright=x;self.i2c.writeto_mem(self.addr,_regBright,bytes([self.bright]));sleep_ms(1)
+	def clear(self):d=self.i2c.readfrom_mem(self.addr,_regCtrl,1);r=int.from_bytes(d,_A);sleep_ms(1);r|=1<<0;self.i2c.writeto_mem(self.addr,_regCtrl,bytes([r]));sleep_ms(1)
 	def setI2Caddr(self,newAddr):
-		if newAddr>=8 and newAddr<=119:self.i2c.writeto_mem(self.addr,_regI2cAddr,bytes([newAddr]));self.addr=newAddr
+		if newAddr>=8 and newAddr<=119:self.i2c.writeto_mem(self.addr,_regI2cAddr,bytes([newAddr]));self.addr=newAddr;sleep_ms(5)
 		else:print('address must be >=0x08 and <=0x77')
 	def readFirmware(self):v=self.i2c.readfrom_mem(self.addr,_regFirmVer,2);return v[1],v[0]
 	def readID(self):return self.i2c.readfrom_mem(self.addr,_regDevID,1)[0]
 	def pwrLED(self,bit):
-		if bit==False:b=0
-		else:b=1
-		self.i2c.writeto_mem(self.addr,_regCtrl,bytes([b<<1]))
+		d=self.i2c.readfrom_mem(self.addr,_regCtrl,1);d=int.from_bytes(d,_A);sleep_ms(1)
+		if bit==False:d&=~(1<<1)
+		else:d|=1<<1
+		self.i2c.writeto_mem(self.addr,_regCtrl,bytes([d]));sleep_ms(1)
+	def fill(self,c):
+		for i in range(len(self.led)):self.led[i]=c
+		self.show()
 	def __init__(self,addr=_baseAddr,i2c=i2c,bright=50):
 		self.i2c=i2c;a=addr
 		if type(a)is list:self.addr=_baseAddr+a[0]+2*a[1]+4*a[2]+8*a[3]
