330abb6f13ea6cca60d1b9efa60597f9ef4d7359
GitHub Action <action@github.com>
2021-10-08 06:34:49 +0000

    Update min/PiicoDev_RGB.py

--- min/PiicoDev_RGB.py
+++ min/PiicoDev_RGB.py
@@ -1,7 +1,6 @@
-_A='big'
+_A=None
 from PiicoDev_Unified import *
-i2c=PiicoDev_Unified_I2C()
-_baseAddr=32
+_baseAddr=8
 _DevID=132
 _regDevID=0
 _regFirmVer=1
@@ -21,24 +20,24 @@ def hsv_to_rgb(h,s=1,v=1):
 	if i==5:return[v,p,q]
 class PiicoDev_RGB:
 	def setPixelColor(self,n,r,g,b):self.led[n]=[r,g,b]
-	def show(self):buffer=bytes(self.led[0])+bytes(self.led[1])+bytes(self.led[2]);i2c.writeto_mem(self.addr,_regLedVals,buffer)
+	def show(self):buffer=bytes(self.led[0])+bytes(self.led[1])+bytes(self.led[2]);self.i2c.writeto_mem(self.addr,_regLedVals,buffer)
 	def setBrightness(self,x):self.bright=x;self.i2c.writeto_mem(self.addr,_regBright,bytes([self.bright]));sleep_ms(1)
-	def clear(self):d=self.i2c.readfrom_mem(self.addr,_regCtrl,1);r=int.from_bytes(d,_A);sleep_ms(1);r|=1<<0;self.i2c.writeto_mem(self.addr,_regCtrl,bytes([r]));sleep_ms(1)
+	def clear(self):d=self.i2c.readfrom_mem(self.addr,_regCtrl,1);r=int.from_bytes(d,'big');sleep_ms(1);r|=1<<0;self.i2c.writeto_mem(self.addr,_regCtrl,bytes([r]));sleep_ms(1)
 	def setI2Caddr(self,newAddr):
 		if newAddr>=8 and newAddr<=119:self.i2c.writeto_mem(self.addr,_regI2cAddr,bytes([newAddr]));self.addr=newAddr;sleep_ms(5)
 		else:print('address must be >=0x08 and <=0x77')
 	def readFirmware(self):v=self.i2c.readfrom_mem(self.addr,_regFirmVer,2);return v[1],v[0]
 	def readID(self):return self.i2c.readfrom_mem(self.addr,_regDevID,1)[0]
 	def pwrLED(self,bit):
-		d=self.i2c.readfrom_mem(self.addr,_regCtrl,1);d=int.from_bytes(d,_A);sleep_ms(1)
+		d=self.i2c.readfrom_mem(self.addr,_regCtrl,1);d=int.from_bytes(d,'big');sleep_ms(1)
 		if bit==False:d&=~(1<<1)
 		else:d|=1<<1
 		self.i2c.writeto_mem(self.addr,_regCtrl,bytes([d]));sleep_ms(1)
 	def fill(self,c):
 		for i in range(len(self.led)):self.led[i]=c
 		self.show()
-	def __init__(self,addr=_baseAddr,i2c=i2c,bright=50):
-		self.i2c=i2c;a=addr
+	def __init__(self,bus=_A,freq=_A,sda=_A,scl=_A,addr=_baseAddr,bright=50):
+		self.i2c=create_unified_i2c(bus=bus,freq=freq,sda=sda,scl=scl);print('pass');a=addr
 		if type(a)is list:self.addr=_baseAddr+a[0]+2*a[1]+4*a[2]+8*a[3]
 		else:self.addr=a
 		self.led=[[0,0,0],[0,0,0],[0,0,0]];self.bright=bright
